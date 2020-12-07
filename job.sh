41b15a46e9f7049fd24eae95fefc3af941634b5d
uduse <i@uduse.com>
2020-12-07 00:26:15 -0800

    Update.

--- job.sh
+++ job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=10
+#SBATCH --time=7
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=1G
 #SBATCH --cpus-per-task=1
-#SBATCH --array=1-4
+#SBATCH --array=1-64
 #SBATCH --output=tmp-%A_%a.out
 ##SBATCH --output=/dev/null
 
@@ -27,8 +27,8 @@ else
 	done
 fi
 
-EPOCHS=10
-STEPS_PER_EPOCH=2000
+EPOCHS=20
+STEPS_PER_EPOCH=4000
 python script.py --epochs $EPOCHS --steps_per_epoch $STEPS_PER_EPOCH --algo vpg --seed $SLURM_ARRAY_JOB_ID  --seed_weight_init $SLURM_ARRAY_TASK_ID > log/$DIR_NAME/${SLURM_ARRAY_TASK_ID}_vpg.out
 python script.py --epochs $EPOCHS --steps_per_epoch $STEPS_PER_EPOCH --algo ppo --seed $SLURM_ARRAY_JOB_ID  --seed_weight_init $SLURM_ARRAY_TASK_ID > log/$DIR_NAME/${SLURM_ARRAY_TASK_ID}_ppo_c.out
 python script.py --epochs $EPOCHS --steps_per_epoch $STEPS_PER_EPOCH --algo ppo --seed $SLURM_ARRAY_JOB_ID  --seed_weight_init $((${SLURM_ARRAY_TASK_ID} + 1000)) > log/$DIR_NAME/${SLURM_ARRAY_TASK_ID}_ppo_uc.out
